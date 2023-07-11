02fa3ece80f3a33f7afe3d38344d438bf9cf68b9
xBRAZENx <guptaatharva727@gmail.com>
2023-07-11 12:13:40 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppMethod
+{
+    public class Emp
+    {
+        public string FullName(string fname,string lname)
+        {
+            return fname+" "+lname;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Emp emp = new Emp();
+
+            Console.WriteLine(emp.FullName("Sam", "Dicosta"));
+            Console.ReadKey();
+        }
+    }
+}
